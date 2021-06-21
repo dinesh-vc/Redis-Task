9068a0e0439a1bde2ee3f9b8d7e437fdb689cbef
DineshViitor <dinesh.chavda@viitorcloud.co>
2021-06-21 18:32:15 +0530

    INCR Function Implimented

--- src/app/controller/controller.js
+++ src/app/controller/controller.js
@@ -1,19 +1,7 @@
 var redis = require("redis");
 var client = redis.createClient();
 
-
-let blueCount=0;
-let yellowCount=0;
-let redCount=0;
-let greyCount=0;
-
 client.on("connect", function () {
-      
- blueCount=0;
- yellowCount=0;
- redCount=0;
- greyCount=0;
-
     console.log("Connection Successful!!");
 });
 
@@ -24,47 +12,38 @@ exports.home = async (req, res) => {
 
 exports.blue = async (req, res) => {
 
-    if(blueCount==0) {
-        client.hmset("colorBlue", {
-            color: "Blue",
-            counter: 1
-        }); 
-    }
     
     client.hgetall("colorBlue", (err, data) => {
         if (err) console.log(err);
         else {
-            let count = parseInt(data.counter) + 1;
-            client.hmset("colorBlue", {
-                color: "Blue",
-                counter: count
+
+            client.incr('counterBlue', function(err, counter) {
+                client.hmset('colorBlue',{
+                    color: "Blue",
+                    counter: counter
+                });
             });
         }
     });
+
     client.hgetall("colorBlue", (err, data) => {
         if (err) console.log(err);
         else res.send(data)  
     });
-    blueCount=blueCount+1;
+    
 }
 
 exports.yellow = async (req, res) => {
-
-    if(yellowCount==0) {
-        client.hmset("colorYellow", {
-            color: "yellow",
-            counter: 1
-        }); 
-    }
     
     client.hgetall("colorYellow", (err, data) => {
-
         if (err) console.log(err);
         else {
-            let count = parseInt(data.counter) + 1;
-            client.hmset("colorYellow", {
-                color: "yellow",
-                counter: count
+            client.incr('counterYellow', function(err, counter) {
+                
+                client.hmset('colorYellow',{
+                    color: "Yellow",
+                    counter: counter
+                });
             });
         }
     });
@@ -72,26 +51,20 @@ exports.yellow = async (req, res) => {
         if (err) console.log(err);
         else res.send(data)
     });
-
-    yellowCount=yellowCount+1;
 }
 
 exports.grey = async (req, res) => {
-    
-    if(greyCount==0) {
-        client.hmset("colorGrey", {
-            color: "yellow",
-            counter: 1
-        }); 
-    }
+
 
     client.hgetall("colorGrey", (err, data) => {
         if (err) console.log(err);
         else {
-            let count = parseInt(data.counter) + 1;
-            client.hmset("colorGrey", {
-                color: "Grey",
-                counter: count
+
+            client.incr('counterGrey', function(err, counter) {
+                client.hmset('colorGrey',{
+                    color: "Grey",
+                    counter: counter
+                });
             });
         }
     });
@@ -100,23 +73,18 @@ exports.grey = async (req, res) => {
         if (err) console.log(err);
         else res.send(data)
     });
-    greyCount=greyCount+1;
 }
 exports.red = async (req, res) => {
     
-    if(redCount==0) {
-        client.hmset("colorRed", {
-            color: "Red",
-            counter: 1
-        }); 
-    }
     client.hgetall("colorRed", (err, data) => {
         if (err) console.log(err);
         else {
-            let count = parseInt(data.counter) + 1;
-            client.hmset("colorRed", {
-                color: "Red",
-                counter: count
+
+            client.incr('counterRed', function(err, counter) {
+                client.hmset('colorRed',{
+                    color: "Red",
+                    counter: counter
+                });
             });
         }
     });
@@ -125,5 +93,5 @@ exports.red = async (req, res) => {
         if (err) console.log(err);
         else res.send(data)
     });
-    redCount=redCount+1;
+
 }
