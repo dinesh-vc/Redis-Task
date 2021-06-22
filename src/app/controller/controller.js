fa883bb643534fd2a0910e714ec5a23a7f59b378
DineshViitor <dinesh.chavda@viitorcloud.co>
2021-06-22 17:39:48 +0530

    Code Reduce

--- src/app/controller/controller.js
+++ src/app/controller/controller.js
@@ -6,82 +6,50 @@ client.on("connect", function () {
 });
 
 exports.home = async (req, res) => {
-  
     res.render('home')
 }
 
 exports.blue = async (req, res) => {
 
-    
-    client.hgetall("colorBlue", (err, data) => {
-        if (err) console.log(err);
-        else {
-
-            client.incr('counterBlue', function(err, counter) {
-                client.hmset('colorBlue',{
+            client.incr('counterBlue', function (err, counter) {
+                res.send({
                     color: "Blue",
                     counter: counter
-                });
-            });
-        }
+                })
     });
 
-    client.hgetall("colorBlue", (err, data) => {
-        if (err) console.log(err);
-        else res.send(data)  
-    });
-    
+
 }
 
 exports.yellow = async (req, res) => {
-    
-    client.hgetall("colorYellow", (err, data) => {
-        if (err) console.log(err);
-        else {
-            client.incr('counterYellow', function(err, counter) {
-                
-                client.hmset('colorYellow',{
-                    color: "Yellow",
-                    counter: counter
-                });
-            });
-        }
-    });
-    client.hgetall("colorYellow", (err, data) => {
-        if (err) console.log(err);
-        else res.send(data)
-    });
-}
 
+    client.incr('counterYellow', function (err, counter) {
+        res.send({
+            color: "Yellow",
+            counter: counter
+        })
+});
+   
+}
 exports.grey = async (req, res) => {
 
-
-    client.hgetall("colorGrey", (err, data) => {
-        if (err) console.log(err);
-        else {
-
-            client.incr('counterGrey', function(err, counter) {
-                client.hmset('colorGrey',{
-                    color: "Grey",
-                    counter: counter
-                });
-            });
-        }
-    });
-
-    client.hgetall("colorGrey", (err, data) => {
-        if (err) console.log(err);
-        else res.send(data)
-    });
+    client.incr('counterGrey', function (err, counter) {
+        res.send({
+            color: "Grey",
+            counter: counter
+        })
+});
 }
+
+// using redis object
+
 exports.red = async (req, res) => {
-    
+
     client.hgetall("colorRed", (err, data) => {
         if (err) console.log(err);
         else {
-
-            client.incr('counterRed', function(err, counter) {
-                client.hmset('colorRed',{
+            client.incr('counterRed', function (err, counter) {
+                client.hmset('colorRed', {
                     color: "Red",
                     counter: counter
                 });
